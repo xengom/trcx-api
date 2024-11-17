06e00361d502fbc4c5153603f4a8e9a50224f8dc
dec <xengom@gmail.com>
2024-11-17 19:50:10 +0900

    Basic GraphQL resolver

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import {ApolloServer, gql} from "apollo-server";
 
-const tweets = [
+let tweets = [
   {
     id: "1",
     text: "hello",
@@ -56,6 +56,22 @@ const resolvers = {
     tweet(root, {id}) {
       return tweets.find(tweet => tweet.id === id);
     }
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, {id}) {
+      const target = tweets.find(tweet => tweet.id === id);
+      if (!target) return false;
+      tweets = tweets.filter(tweet => tweet.id !== id);
+      return true;
+    },
   }
 }
 
