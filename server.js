768176cdf79256f67faf639e26b8cac8d3c1c693
dec <xengom@gmail.com>
2024-11-17 19:16:51 +0900

    Basic GraphQL resolver

--- server.js
+++ server.js
@@ -1,14 +1,65 @@
 import {ApolloServer, gql} from "apollo-server";
 
-const typeDefs = gql`
-  type Query {
-    text: String
-    hello: String
-    
+const tweets = [
+  {
+    id: "1",
+    text: "hello",
+    author: {
+      id: "1",
+      username: "John"
+    },
   },
+  {
+    id: "2",
+    text: "hello2",
+    author: {
+      id: "2",
+      username: "John2"
+    },
+  },
+  {
+    id: "3",
+    text: "hello3",
+    author: {
+      id: "3",
+      username: "John3"
+    },
+  },
+]
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+    photo: String
+    author: User!
+  }
+  type Query {
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
 `
 
-const server = new ApolloServer({typeDefs})
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, {id}) {
+      return tweets.find(tweet => tweet.id === id);
+    }
+  }
+}
+
+const server = new ApolloServer({typeDefs, resolvers});
 
 server.listen().then(({url}) => {
   console.log(`Running on ${url}`);
